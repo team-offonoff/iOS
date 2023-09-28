c7e5a20b269c98a64bf118bf5395e10ca7f1c3a1
SSOOYA <bsy24660@naver.com>
2023-09-28 13:46:06 +0900

    [chore] #3: 폰트 설정 주석 제거 및 폰트 파일 추가

--- Plugins/EnvironmentPlugin/ProjectDescriptionHelpers/InfoPlists.swift
+++ Plugins/EnvironmentPlugin/ProjectDescriptionHelpers/InfoPlists.swift
@@ -20,21 +20,16 @@ public extension Project {
             ]
         ],
         "UIAppFonts": [
-            // FIXME: - 폰트 추가 후 수정
-            //                "Item 0": "Pretendard-Black.otf",
-            //                "Item 1": "Pretendard-Bold.otf",
-            //                "Item 2": "Pretendard-ExtraBold.otf",
-            //                "Item 3": "Pretendard-ExtraLight.otf",
-            //                "Item 4": "Pretendard-Light.otf",
-            //                "Item 5": "Pretendard-Medium.otf",
-            //                "Item 6": "Pretendard-Regular.otf",
-            //                "Item 7": "Pretendard-SemiBold.otf",
-            //                "Item 8": "Pretendard-Thin.otf"
+            "Item 0": "Pretendard-Black.otf",
+            "Item 1": "Pretendard-Bold.otf",
+            "Item 2": "Pretendard-ExtraBold.otf",
+            "Item 5": "Pretendard-Medium.otf",
+            "Item 6": "Pretendard-Regular.otf",
+            "Item 7": "Pretendard-SemiBold.otf",
         ],
         "App Transport Security Settings": ["Allow Arbitrary Loads": true],
         "NSAppTransportSecurity": ["NSAllowsArbitraryLoads": true],
         "ITSAppUsesNonExemptEncryption": false,
-//        "UIUserInterfaceStyle": "Dark",
         "NSPhotoLibraryUsageDescription": "갤러리 권한 작성하기"
     ]
     
