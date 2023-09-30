e9ba339d94ed328acaa18334fc2ce31bf671fa65
SSOOYA <bsy24660@naver.com>
2023-09-30 18:44:03 +0900

    [feat] #2: 인터페이스 역할인 TabCoordinator 프로토콜과 구현체인 DefaultTabCoordinator 구현

---
+++ Projects/Features/RootFeature/Sources/TabBar/DefaultTabCoordinator.swift
@@ -0,0 +1,79 @@
+//
+//  DefaultTabCoordinator.swift
+//  RootFeature
+//
+//  Created by 박소윤 on 2023/09/26.
+//  Copyright © 2023 AB. All rights reserved.
+//
+
+import UIKit
+
+import FeatureDependency
+import HomeFeature
+
+public class DefaultTabCoordinator: TabCoordinator {
+    
+    private let window: UIWindow?
+    private let tabBarController: UITabBarController
+    private let homeTabNavigationController: UINavigationController
+    private let abTabNavigationController: UINavigationController
+    private let newTabNavigationController: UINavigationController
+    private let userTabNavigationController: UINavigationController
+    
+    public init(
+        window: UIWindow?
+    ){
+        self.window = window
+        self.tabBarController = TabBarController()
+        self.homeTabNavigationController = UINavigationController()
+        self.abTabNavigationController = UINavigationController()
+        self.newTabNavigationController = UINavigationController()
+        self.userTabNavigationController = UINavigationController()
+    }
+    
+    public func start() {
+        window?.rootViewController = tabBarController
+        setTabBarViewControllers()
+        startHome()
+        startAB()
+        startNew()
+        startUser()
+    }
+    
+    private func setTabBarViewControllers(){
+        
+        let tabs: [UINavigationController] = [
+            homeTabNavigationController,
+            abTabNavigationController,
+            newTabNavigationController,
+            userTabNavigationController
+        ]
+        
+        TabBarItem.allCases.forEach {
+            tabs[$0.rawValue].tabBarItem = $0.asTabBarItem()
+        }
+        
+        tabBarController.setViewControllers(tabs, animated: true)
+        tabBarController.selectedIndex = 0
+    }
+    
+    private func startHome() {
+        let homeCoordinator: HomeCoordinator = DefaultHomeCoordinator(navigationController: homeTabNavigationController)
+        homeCoordinator.start()
+    }
+    
+    private func startAB() {
+//        let abCoordinator: HomeCoordinator = DefaultHomeCoordinator(navigationController: abTabNavigationController)
+//        abCoordinator.start()
+    }
+    
+    private func startNew(){
+//        let newCoordinator: HomeCoordinator = DefaultHomeCoordinator(navigationController: newTabNavigationController)
+//        newCoordinator.start()
+    }
+    
+    private func startUser(){
+//        let userCoordinator: HomeCoordinator = DefaultHomeCoordinator(navigationController: userTabNavigationController)
+//        userCoordinator.start()
+    }
+}
