5b4d17af6959da7451e9f70f2c065d8d0bb7998b
SSOOYA <bsy24660@naver.com>
2023-10-03 13:46:16 +0900

    [feat] #5: HomeTabViewController 구현

---
+++ Projects/Features/HomeFeature/Sources/ViewController/HomeTabViewController.swift
@@ -0,0 +1,52 @@
+//
+//  HomeTabViewController.swift
+//  HomeFeature
+//
+//  Created by 박소윤 on 2023/09/25.
+//  Copyright © 2023 AB. All rights reserved.
+//
+
+import Foundation
+
+import ABKit
+import UIKit
+
+import FeatureDependency
+
+final class HomeTabViewController: BaseViewController<HeaderView, HomeTabView, DefaultHomeCoordinator>{
+    
+    private let viewModel: HomeTabViewModel
+
+    init(){
+        self.viewModel = DefaultHomeTabViewModel()
+        super.init(
+            headerView: HeaderView(icon: Image.homeAlarmOff, selectedIcon: Image.homeAlarmOn),
+            mainView: HomeTabView()
+        )
+    }
+
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        mainView.setBottomSheetDefaultY()
+    }
+    
+    public override func style() {
+        view.backgroundColor = Color.homeBackground
+    }
+    
+    override func bind() {
+        bindBottomSheetMove()
+    }
+    
+    private func bindBottomSheetMove(){
+        viewModel.canBottomSheetMovePublisher
+            .receive(on: RunLoop.main)
+            .sink{ canMove in
+                print(canMove)
+                self.mainView.chatBottomSheetFrame.isUserInteractionEnabled = canMove
+            }.store(in: &cancellables)
+    }
+}
