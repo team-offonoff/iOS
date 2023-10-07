f07d3c2d8204e5a40c2880353e95f917b90eb593
SSOOYA <bsy24660@naver.com>
2023-10-07 13:15:50 +0900

    [feat] #3: Device 구조체 추가 > 디바이스 너비, 높이 변수 설정

---
+++ Projects/ABKit/Sources/Utils/Device.swift
@@ -0,0 +1,14 @@
+//
+//  Device.swift
+//  ABKit
+//
+//  Created by 박소윤 on 2023/10/04.
+//  Copyright © 2023 AB. All rights reserved.
+//
+
+import UIKit
+
+public struct Device {
+    public static let width = UIScreen.main.bounds.size.width
+    public static let height = UIScreen.main.bounds.size.height
+}
