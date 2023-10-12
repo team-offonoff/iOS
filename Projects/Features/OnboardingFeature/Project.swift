9f1eb962255d20bd34b10278a95f686f8a604b53
SSOOYA <bsy24660@naver.com>
2023-10-12 16:53:37 +0900

    [feat] #1: OnboardingFeature 프레임워크 세팅

--- Projects/Features/OnboardingFeature/Project.swift
+++ Projects/Features/OnboardingFeature/Project.swift
@@ -3,7 +3,7 @@ import ProjectDescriptionHelpers
 import DependencyPlugin
 
 let project = Project.makeModule(
-    name: "BFeature",
+    name: "OnboardingFeature",
     targets: [.unitTest, .staticFramework, .demo],
     internalDependencies: [
         .Features.FeatureDependency
