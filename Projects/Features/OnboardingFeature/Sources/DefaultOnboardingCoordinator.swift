51d7f0fd92046e3ae86f23748e79f33d5e2d2696
SSOOYA <bsy24660@naver.com>
2023-10-15 15:37:15 +0900

    [feat] #9: OnboardingCoordinator 구현

---
+++ Projects/Features/OnboardingFeature/Sources/DefaultOnboardingCoordinator.swift
@@ -0,0 +1,34 @@
+//
+//  DefaultOnboardingCoordinator.swift
+//  OnboardingFeatureDemo
+//
+//  Created by 박소윤 on 2023/10/12.
+//  Copyright © 2023 AB. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+import FeatureDependency
+
+public class DefaultOnboardingCoordinator: OnboardingCoordinator {
+    
+    private var window: UIWindow?
+    private let navigationController: UINavigationController
+    
+    public init(window: UIWindow?){
+        self.window = window
+        self.navigationController = UINavigationController()
+        self.window?.rootViewController = navigationController
+    }
+    
+    public init(navigationController: UINavigationController){
+        self.navigationController = navigationController
+    }
+    
+    public func start() {
+        let viewController = LoginViewController()
+        viewController.coordinator = self
+        navigationController.pushViewController(viewController, animated: true)
+    }
+}
