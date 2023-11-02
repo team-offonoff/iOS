36c9db7cd537c9fdacd5d5eb1b8b316af48a8499
SSOOYA <bsy24660@naver.com>
2023-11-02 13:12:50 +0900

    [chore] #1: developer team null 설정

--- Plugins/EnvironmentPlugin/ProjectDescriptionHelpers/SettingsDictionary+.swift
+++ Plugins/EnvironmentPlugin/ProjectDescriptionHelpers/SettingsDictionary+.swift
@@ -59,7 +59,7 @@ public extension SettingsDictionary {
     
     func setCodeSignManual() -> SettingsDictionary {
         merging(["CODE_SIGN_STYLE": SettingValue(stringLiteral: "Manual")])
-            .merging(["DEVELOPMENT_TEAM": SettingValue(stringLiteral: "9K86FQHDLU")])
+            .merging(["DEVELOPMENT_TEAM": SettingValue(stringLiteral: "")])
             .merging(["CODE_SIGN_IDENTITY": SettingValue(stringLiteral: "$(CODE_SIGN_IDENTITY)")])
     }
     
