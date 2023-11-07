d7341030ec02d871d5f780b5e5c161cb4b78d1cb
SSOOYA <bsy24660@naver.com>
2023-11-07 13:33:46 +0900

    [feat] #16: topic repository 인터페이스 및 구현체 구현

---
+++ Projects/Data/Sources/Repository/DefaultTopicRepository.swift
@@ -0,0 +1,67 @@
+//
+//  DefaultTopicRepository.swift
+//  Data
+//
+//  Created by 박소윤 on 2023/11/03.
+//  Copyright © 2023 AB. All rights reserved.
+//
+
+import Foundation
+import Domain
+import Core
+
+public final class DefaultTopicRepository: TopicRepository {
+    
+    private let networkService: NetworkService = NetworkService.shared
+    
+    public init() { }
+    
+    public func generateTopic(request: Topic) -> NetworkResultPublisher<Topic?> {
+        
+        var urlComponents = networkService.baseUrlComponents
+        urlComponents?.path = "/topics"
+        
+        guard let requestBody = try? JSONEncoder().encode(makeDTO()),
+              let urlRequest = urlComponents?.toURLRequest(method: .post, httpBody: requestBody) else {
+            fatalError("json encoding or url parsing error")
+        }
+        
+        return dataTask(request: urlRequest, responseType: TopicResponseDTO.self)
+        
+        func makeDTO() -> TopicGenerateRequestDTO {
+            .init(
+                topicSide: request.side.rawValue,
+                categoryId: request.categoryId,
+                topicTitle: request.title,
+                choices: makeChoiceDTO(),
+                deadline: request.deadline
+            )
+        }
+        
+        func makeChoiceDTO() -> [ChoiceGenerateRequestDTO] {
+            request.choices.map{ choice in
+                    .init(
+                        choiceOption: choice.option.rawValue,
+                        choiceContentRequest: .init(
+                            type: "IMAGE_TEXT",
+                            imageUrl: String(describing: choice.content.imageURL),
+                            text: choice.content.text
+                        )
+                    )
+            }
+        }
+    }
+    
+    //임시 구현
+    public func fetchTopic() -> NetworkResultPublisher<[Topic]> {
+        
+        var urlComponents = networkService.baseUrlComponents
+        urlComponents?.path = "/topics"
+        
+        guard let urlRequest = urlComponents?.toURLRequest(method: .get) else {
+            fatalError("json encoding or url parsing error")
+        }
+        
+        return arrayDataTask(request: urlRequest, elementType: TopicResponseDTO.self)
+    }
+}
---
+++ Projects/Domain/Sources/RepositoryInterface/TopicRepository.swift
@@ -0,0 +1,15 @@
+//
+//  TopicRepository.swift
+//  Domain
+//
+//  Created by 박소윤 on 2023/11/03.
+//  Copyright © 2023 AB. All rights reserved.
+//
+
+import Foundation
+import Core
+
+public protocol TopicRepository: Repository{
+    func generateTopic(request: Topic) -> NetworkResultPublisher<Topic?>
+    func fetchTopic() -> NetworkResultPublisher<[Topic]>
+}
