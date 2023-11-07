28169030e72d11920ff9da91a550ac703d363669
SSOOYA <bsy24660@naver.com>
2023-11-07 13:32:05 +0900

    [feat] #16: TopicGenerateRequestDTO와 TopicResponseDTO 구조체 추가

---
+++ Projects/Data/Sources/DTO/Topic/TopicGenerateRequestDTO.swift
@@ -0,0 +1,29 @@
+//
+//  TopicGenerateRequestDTO.swift
+//  Data
+//
+//  Created by 박소윤 on 2023/11/03.
+//  Copyright © 2023 AB. All rights reserved.
+//
+
+import Foundation
+import Domain
+
+struct TopicGenerateRequestDTO: Encodable {
+    let topicSide: String
+    let categoryId: Int
+    let topicTitle: String
+    let choices: [ChoiceGenerateRequestDTO]
+    let deadline: Int
+}
+
+struct ChoiceGenerateRequestDTO: Encodable {
+    let choiceOption: String
+    let choiceContentRequest: ChoiceContentRequestModel
+}
+
+struct ChoiceContentRequestModel: Encodable {
+    let type: String
+    let imageUrl: String
+    let text: String
+}
