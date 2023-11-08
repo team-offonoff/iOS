8ed1d1cc527aafc41cdc223d7b71197ea1c537a6
SSOOYA <bsy24660@naver.com>
2023-11-08 14:18:35 +0900

    feat(Interface): create TopicPageControllable, TimerControllable and HomeTabViewModel adopt (#17)

--- Projects/Features/HomeFeature/Interface/ViewModel/HomeTabViewModel.swift
+++ Projects/Features/HomeFeature/Interface/ViewModel/HomeTabViewModel.swift
@@ -8,8 +8,26 @@
 
 import Foundation
 import Combine
+import Domain
+import FeatureDependency
 
-public protocol HomeTabViewModel {
+public protocol HomeTabViewModel: TopicPageControllable, TimerControllable {
     var canBottomSheetMovePublisher: Published<Bool>.Publisher { get }
     func viewDidLoad()
+}
+
+public protocol TopicPageControllable {
+    var topics: [Topic] { get }
+    var canMovePrevious: Bool { get }
+    var canMoveNext: Bool { get }
+    var willMovePage: Published<IndexPath>.Publisher { get }
+    var reloadTopics: PassthroughSubject<Void, Never> { get }
+    func moveNextTopic()
+    func movePreviousTopic()
+}
+
+public protocol TimerControllable{
+    var timerSubject: PassthroughSubject<TimerInfo, Never> { get }
+    func startTimer()
+    func stopTimer()
 }
