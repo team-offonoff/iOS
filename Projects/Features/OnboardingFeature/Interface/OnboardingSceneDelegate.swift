30b279d23c7795d09ec7e0cd7af7ab9197cedee6
SSOOYA <bsy24660@naver.com>
2023-11-19 14:28:36 +0900

    feat(Interface): create OnboardingSceneDelegate (#27)

---
+++ Projects/Features/OnboardingFeature/Interface/OnboardingSceneDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingSceneDelegate.swift
+//  OnboardingFeatureInterface
+//
+//  Created by 박소윤 on 2023/11/19.
+//  Copyright © 2023 AB. All rights reserved.
+//
+
+import Foundation
+
+public protocol OnboardingSceneDelegate {
+    func sceneMoveToRootFeature()
+}
+
