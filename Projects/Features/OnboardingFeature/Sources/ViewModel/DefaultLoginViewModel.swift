578eddd5e3ce6d9fb86e30b1d655847ce0f760e7
SSOOYA <bsy24660@naver.com>
2023-11-19 13:49:00 +0900

    feat(ViewModel): implement LoginViewModelOutput and connect login use case (#26)

--- Projects/Features/OnboardingFeature/Sources/ViewModel/DefaultLoginViewModel.swift
+++ Projects/Features/OnboardingFeature/Sources/ViewModel/DefaultLoginViewModel.swift
@@ -8,27 +8,70 @@
 
 import Foundation
 import KakaoSDKUser
+import KakaoSDKAuth
 import AuthenticationServices
 import OnboardingFeatureInterface
+import FeatureDependency
 import Domain
 import Combine
+import Core
 
-final class DefaultLoginViewModel: LoginViewModel {
-    
-    @Published private var kakaoUser: KakaoSDKUser.User?
-    
-    private let loginUseCase: any LoginUseCase
+final class DefaultLoginViewModel: BaseViewModel, LoginViewModel {
     
     init(
         loginUseCase: any LoginUseCase
     ){
         self.loginUseCase = loginUseCase
+        super.init()
         bind()
     }
     
-    private func bind(){
-//        $kakaoUser
+    let moveHome: PassthroughSubject<Void, Never> = PassthroughSubject()
+    let moveSignUp: PassthroughSubject<Void, Never> = PassthroughSubject()
     
+    @Published private var kakaoUser: (oauthToken: KakaoSDKAuth.OAuthToken, user: KakaoSDKUser.User)?
+    
+    private let loginUseCase: any LoginUseCase
+    
+    override func bind(){
+        bindKakaoLogin()
+    }
+    
+    private func bindKakaoLogin() {
+        
+        let response = $kakaoUser
+            .compactMap{ $0?.oauthToken.idToken }
+            .flatMap{ idToken in
+                self.loginUseCase.execute(request: .init(idToken: idToken))
+            }
+        
+        // response success
+        response
+            .filter{ $0.code == .success }
+            .compactMap{ $0 }
+            .compactMap{ $0.data }
+            .sink{ [weak self] in
+                
+                guard let isNewMember = $0.isNewMember,
+                      let accessToken = $0.accessToken else { return }
+                
+                UserManager.shared.accessToken = accessToken
+                UserManager.shared.idToken = self?.kakaoUser?.oauthToken.idToken
+                UserManager.shared.email = self?.kakaoUser?.user.kakaoAccount?.email
+                
+                if isNewMember {
+                    self?.moveSignUp.send(())
+                }
+                else {
+                    self?.moveHome.send(())
+                }
+            }
+            .store(in: &cancellable)
+        
+        // response fail
+        let failResponse = response
+            .filter{ $0.code != .success }
+        
     }
     
     //MARK: Kakao Login
@@ -41,7 +84,6 @@ final class DefaultLoginViewModel: LoginViewModel {
                 } else {
                     print("loginWithKakaoTalk() success.")
                     self.requestKakaoUserInformation()
-                    print("kakao token:",oauthToken?.idToken)
                 }
             }
         } else {
@@ -51,7 +93,6 @@ final class DefaultLoginViewModel: LoginViewModel {
                 } else {
                     print("loginWithKakaoAccount() success.")
                     self.requestKakaoUserInformation()
-                    print("kakao token:",oauthToken?.idToken)
                 }
             }
         }
@@ -61,30 +102,37 @@ final class DefaultLoginViewModel: LoginViewModel {
         UserApi.shared.me() { (user, error) in
             if let error = error {
                 print(error)
-            } else {
+            }
+            else {
                 if let user = user {
                     let scopes = makeKakaoRequestScopes(user: user)
                     if scopes.count > 0 {
                         print("사용자에게 추가 동의를 받아야 합니다.")
                         //scope 목록을 전달하여 카카오 로그인 요청
-                        UserApi.shared.loginWithKakaoAccount(scopes: scopes) { (_, error) in
+                        UserApi.shared.loginWithKakaoAccount(scopes: scopes) { (oauthToken, error) in
                             if let error = error {
                                 print(error)
-                            } else {
+                            }
+                            else {
                                 UserApi.shared.me() { (user, error) in
                                     if let error = error {
                                         print(error)
-                                    } else {
+                                    }
+                                    else {
                                         print("me() success.")
-                                        self.kakaoUser = user
+                                        guard let oauthToken = oauthToken, let user = user else { return }
+                                        self.kakaoUser = (oauthToken, user)
                                     }
                                 }
                             }
                         }
-                    } else {
-                        print("사용자의 추가 동의가 필요하지 않습니다.")
-                        self.kakaoUser = user
                     }
+                    /*
+                     else {
+                     print("사용자의 추가 동의가 필요하지 않습니다.")
+                     self.kakaoUser = user
+                     }
+                     */
                 }
             }
         }
@@ -111,11 +159,11 @@ final class DefaultLoginViewModel: LoginViewModel {
               let authorizationCode = String(data: authorizationCodeData, encoding: .utf8),
               let identityTokenData = credential.identityToken,
               let identityToken = String(data: identityTokenData, encoding: .ascii),
-              let email = credential.email,
-              let name = credential.fullName
+              let email = credential.email
+//              let name = credential.fullName
         else { return }
         
         let userIdentifier = credential.user
-        let userName = "\(name.familyName!)\(name.givenName!)"
+//        let userName = "\(name.familyName!)\(name.givenName!)"
     }
 }
