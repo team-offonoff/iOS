cc002b903417156617f8e713d90dd4014876dc0b
SSOOYA <bsy24660@naver.com>
2023-12-03 15:24:33 +0900

    feat(Core): create TopicBottomSheetFunction (#41)

---
+++ Projects/Core/Sources/TopicBottomSheetFunction.swift
@@ -0,0 +1,15 @@
+//
+//  TopicBottomSheetFunction.swift
+//  Core
+//
+//  Created by 박소윤 on 2023/12/03.
+//  Copyright © 2023 AB. All rights reserved.
+//
+
+import Foundation
+
+public enum TopicBottomSheetFunction: CaseIterable {
+    case hide
+    case report
+    case reset
+}
