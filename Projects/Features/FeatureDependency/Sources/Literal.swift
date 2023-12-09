6e557a55fc789e13c994425c8dfc0f771bd19ddc
SSOOYA <bsy24660@naver.com>
2023-12-09 13:41:51 +0900

    feat(FeatureDependency): create Literal struct (#52)

---
+++ Projects/Features/FeatureDependency/Sources/Literal.swift
@@ -0,0 +1,18 @@
+//
+//  Literal.swift
+//  FeatureDependency
+//
+//  Created by 박소윤 on 2023/12/09.
+//  Copyright © 2023 AB. All rights reserved.
+//
+
+import Foundation
+
+public struct Literal {
+    
+    public enum BottomSheet {
+        public static let topicBottomSheet = "topicBottomSheet"
+        public static let chatBottomSheet = "chatBottomSheet"
+    }
+    
+}
