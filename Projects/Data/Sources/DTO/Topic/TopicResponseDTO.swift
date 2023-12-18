0e288cb629922af37a7fbf6f2152a13d2479a51b
SSOOYA <bsy24660@naver.com>
2023-12-18 19:01:49 +0900

    feat(DTO): 토픽 응답 모델 업데이트 (#78)

--- Projects/Data/Sources/DTO/Topic/TopicResponseDTO.swift
+++ Projects/Data/Sources/DTO/Topic/TopicResponseDTO.swift
@@ -11,54 +11,98 @@ import Domain
 import Core
 
 struct TopicResponseDTO: Decodable {
+    
     let topicId: Int
     let topicSide: String
     let topicTitle: String
-    let categoryId: Int
-    let choices: [ChoiceResponseDTO]
     let deadline: Int
-}
-
-struct ChoiceResponseDTO: Decodable {
+    let voteCount: Int
+    let topicContent: String?
+    let keyword: KeywordResponseDTO
+    let choices: [ChoiceResponseDTO]
+    let author: AuthorResponseDTO
+    let selectedOption: String?
     
-    struct ContentResponseDTO: Decodable {
-        let text: String
-        let imageURL: String
+    struct KeywordResponseDTO: Decodable {
+        let keywordId: Int
+        let keywordName: String
+        let topicSide: String
     }
     
-    let choiceId: Int
-    let content: ContentResponseDTO
-    let choiceOption: String
+    struct ChoiceResponseDTO: Decodable {
+        let choiceId: Int
+        let content: ContentResponseDTO
+        let choiceOption: String
+        
+        struct ContentResponseDTO: Decodable {
+            let text: String
+            let imageURL: String
+            let type: String
+        }
+    }
+    
+    struct AuthorResponseDTO: Decodable {
+        let id: Int
+        let nickname: String
+        let profileImageUrl: String?
+    }
 }
 
 //MARK: Domainable
 
 extension TopicResponseDTO: Domainable {
+    
     func toDomain() -> Topic {
         .init(
             id: topicId,
-            side: TopicSide(rawValue: topicSide)!,
+            side: Mapper.entity(topicSide: topicSide),
             title: topicTitle,
-            categoryId: categoryId,
+            deadline: deadline,
+            voteCount: voteCount,
+            keyword: keyword.toDomain(),
             choices: choices.map{ $0.toDomain() },
-            deadline: deadline
+            author: author.toDomain(),
+            selectedOption: Mapper.entity(choiceOption: selectedOption)
         )
     }
 }
 
-extension ChoiceResponseDTO: Domainable {
+extension TopicResponseDTO.KeywordResponseDTO: Domainable {
+    func toDomain() -> Keyword {
+        .init(
+            id: keywordId,
+            name: keywordName,
+            topicSide: Mapper.entity(topicSide: topicSide)
+        )
+    }
+}
+
+extension TopicResponseDTO.ChoiceResponseDTO: Domainable {
     func toDomain() -> Choice {
         .init(
             id: choiceId,
             content: content.toDomain(),
-            option: ChoiceOptionDTO(rawValue: choiceOption)!.toDomain()
+            option: Mapper.entity(choiceOption: choiceOption)!
         )
     }
 }
 
-extension ChoiceResponseDTO.ContentResponseDTO: Domainable {
+extension TopicResponseDTO.ChoiceResponseDTO.ContentResponseDTO: Domainable {
     func toDomain() -> Choice.Content {
-        .init(text: text, imageURL: URL(string: imageURL))
+        .init(
+            text: text,
+            imageURL: URL(string: imageURL)
+        )
+    }
+}
+
+extension TopicResponseDTO.AuthorResponseDTO: Domainable {
+    func toDomain() -> Author {
+        .init(
+            id: id,
+            nickname: nickname,
+            profileImageUrl: URL(string: profileImageUrl ?? "")
+        )
     }
 }
 
