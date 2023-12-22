7866a4aa8232e96c01e1699f08404a9582b38cbb
SSOOYA <bsy24660@naver.com>
2023-12-22 15:10:32 +0900

    feat(ABKit): 컬러값 추가

--- Projects/ABKit/Sources/Utils/Color.swift
+++ Projects/ABKit/Sources/Utils/Color.swift
@@ -29,13 +29,13 @@ public struct Color{
     public static let background = UIColor(r: 36, g: 32, b: 54, alpha: 1)
     
     public static let mainA = UIColor(r: 208, g: 67, b: 118)
-    public static let mainA20 = mainA.withAlphaComponent(0.2)
-    
     public static let mainB = UIColor(r: 20, g: 152, b: 170)
-    public static let mainB20 = mainB.withAlphaComponent(0.2)
+    
+    public static let subNavy = UIColor(r: 36, g: 32, b: 54)
+    public static let subNavy2 = UIColor(r: 77, g: 59, b: 124)
     
     public static let subPurple = UIColor(r: 164, g: 111, b: 243)
-    public static let subNavy2 = UIColor(r: 77, g: 59, b: 124)
+    public static let subPurple2 = UIColor(r: 190, g: 40, b: 243)
     
     public static let transparent = UIColor.white.withAlphaComponent(0)
     
@@ -49,4 +49,9 @@ public struct Color{
     public static let black20 = black.withAlphaComponent(0.2)
     public static let black40 = black.withAlphaComponent(0.4)
     public static let black60 = black.withAlphaComponent(0.6)
+    
+    public static let navyWhite20 = UIColor(r: 80, g: 77, b: 94)
+    public static let navyWhite40 = UIColor(r: 124, g: 121, b: 134)
+    public static let navyWhite60 = UIColor(r: 167, g: 166, b: 175)
+    public static let navyWhite80 = UIColor(r: 211, g: 210, b: 215)
 }
