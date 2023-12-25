dfef88f4c6f855c1dad089896161b327ea469f91
SSOOYA <bsy24660@naver.com>
2023-12-25 15:00:34 +0900

    feat: 모달용 헤더뷰 생성

---
+++ Projects/ABKit/Sources/HeaderView/ModalityHeaderView.swift
@@ -0,0 +1,34 @@
+//
+//  ModalityHeaderView.swift
+//  ABKit
+//
+//  Created by 박소윤 on 2023/12/25.
+//  Copyright © 2023 AB. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ModalityHeaderView: HeaderView, ModalDismissable {
+    
+    public lazy var dismissButton: UIButton = {
+       let button = UIButton()
+        button.setImage(Image.modalDismiss, for: .normal)
+        return button
+    }()
+    
+    public override func hierarchy() {
+        super.hierarchy()
+        addSubviews([dismissButton])
+    }
+    
+    public override func layout() {
+        super.layout()
+        dismissButton.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(12)
+            $0.leading.equalToSuperview().offset(20)
+            $0.width.height.equalTo(24)
+            $0.centerY.equalToSuperview()
+        }
+    }
+}
--- Projects/Features/TopicFeature/Demo/Sources/Demo.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Demo.swift
-//  ProjectDescriptionHelpers
-//
-//  Created by 박소윤 on 2023/12/25.
-//
-
-import Foundation
