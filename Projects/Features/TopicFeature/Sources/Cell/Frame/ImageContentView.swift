db188066234bc2c13c9941034bcfa6109f904af6
SSOOYA <bsy24660@naver.com>
2023-12-31 21:41:16 +0900

    style(Cell): ImageContentView input과 output 섹션 구분

--- Projects/Features/TopicFeature/Sources/Cell/Frame/ImageContentView.swift
+++ Projects/Features/TopicFeature/Sources/Cell/Frame/ImageContentView.swift
@@ -16,31 +16,17 @@ extension TopicContentInputTableViewCell {
     
     class ImageContentView: BaseView, ImageTextIncludeContentView {
         
-        var aTextPublisher: AnyPublisher<String, Never> {
-            aTextField.textField.publisher(for: .editingDidEnd)
-        }
-        var bTextPublisher: AnyPublisher<String, Never> {
-            bTextField.textField.publisher(for: .editingDidEnd)
-        }
-        var aImagePublisher: AnyPublisher<UIImage?, Never>? {
-            aImageView.imageSubject.eraseToAnyPublisher()
-        }
-        var bImagePublisher: AnyPublisher<UIImage?, Never>? {
-            bImageView.imageSubject.eraseToAnyPublisher()
-        }
-        
         private let commentLabel: UILabel = {
-           let label = UILabel()
+            let label = UILabel()
             label.text = "가로 세로 길이가 같은 사진을 올리는 것이 좋아요.\n너무 큰 용량의 사진은 화질이 조정될 수 있어요."
             label.textColor = Color.white40
             label.setTypo(Pretendard.regular13, setLineSpacing: true)
             label.numberOfLines = 0
             return label
         }()
-        
         private let imageStackView: UIStackView = UIStackView(axis: .horizontal, spacing: 13)
         private let switchButton: UIButton = {
-           let button = UIButton()
+            let button = UIButton()
             button.setImage(Image.topicGenerateSwitch, for: .normal)
             return button
         }()
@@ -50,6 +36,7 @@ extension TopicContentInputTableViewCell {
         private let textFieldStackView: UIStackView = UIStackView(axis: .vertical, spacing: 10)
         private let aTextField = ImageContentTextField(option: .A)
         private let bTextField = ImageContentTextField(option: .B)
+        
         private var cancellable: Set<AnyCancellable> = []
         
         override func hierarchy() {
@@ -77,19 +64,26 @@ extension TopicContentInputTableViewCell {
         }
         
         override func initialize() {
-            
             [aImageView, bImageView].forEach{
                 $0.isUserInteractionEnabled = true
                 $0.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(imageTap)))
             }
-            
         }
         
         //토픽 생성 ViewController에서 image picker를 띄우도록 알림을 보낸다
         @objc private func imageTap(_ recognizer: UITapGestureRecognizer) {
+            
             guard let view = recognizer.view as? CustomImageView else { return }
-            NotificationCenter.default.post(name: Notification.Name(Topic.Action.showImagePicker.identifier), object: self, userInfo: [Choice.Option.identifier: view.option])
+            
+            NotificationCenter.default
+                .post(
+                    name: Notification.Name(Topic.Action.showImagePicker.identifier),
+                    object: self,
+                    userInfo: [Choice.Option.identifier: view.option]
+                )
         }
+        
+        //MARK: Input
         
         func setLimitCount(_ count: Int?) {
             aTextField.limitCount = count
@@ -98,10 +92,8 @@ extension TopicContentInputTableViewCell {
         
         func setImage(_ image: UIImage, option: Choice.Option) {
             switch option {
-            case .A:
-                aImageView.imageSubject.send(image)
-            case .B:
-                bImageView.imageSubject.send(image)
+            case .A:    aImageView.imageSubject.send(image)
+            case .B:    bImageView.imageSubject.send(image)
             }
         }
         
@@ -123,6 +115,19 @@ extension TopicContentInputTableViewCell {
         
         //MARK: Output
         
+        var aTextPublisher: AnyPublisher<String, Never> {
+            aTextField.textField.publisher(for: .editingDidEnd)
+        }
+        var bTextPublisher: AnyPublisher<String, Never> {
+            bTextField.textField.publisher(for: .editingDidEnd)
+        }
+        var aImagePublisher: AnyPublisher<UIImage?, Never>? {
+            aImageView.imageSubject.eraseToAnyPublisher()
+        }
+        var bImagePublisher: AnyPublisher<UIImage?, Never>? {
+            bImageView.imageSubject.eraseToAnyPublisher()
+        }
+        
         func text(option: Choice.Option) -> String {
             switch option {
             case .A:    return aTextField.textField.text ?? ""
@@ -136,142 +141,156 @@ extension TopicContentInputTableViewCell {
             case .B:    return bImageView.imageSubject.value
             }
         }
-        
-        class CustomImageView: UIImageView {
-            
-            init(option: Choice.Option) {
-                self.option = option
-                super.init(frame: .zero)
-                style()
-                hierarchy()
-                layout()
-                initialize()
-            }
-            
-            required init?(coder: NSCoder) {
-                fatalError("init(coder:) has not been implemented")
-            }
-            
-            let option: Choice.Option
-            let imageSubject: CurrentValueSubject<UIImage?, Never> = CurrentValueSubject(nil)
-            private let uploadLabel: UILabel = {
-                let label = UILabel()
-                label.text = "업로드"
-                label.textColor = Color.subPurple
-                label.setTypo(Pretendard.semibold14)
-                return label
-            }()
-            private let optionLabel: UILabel = {
-                let label = UILabel()
-                label.setTypo(Pretendard.black100)
-                return label
-            }()
-            private let cancelButton: UIButton = {
-                let button = UIButton()
-                button.setImage(Image.topicGenerateImageCancel, for: .normal)
-                button.snp.makeConstraints{
-                    $0.width.height.equalTo(18)
-                }
-                return button
-            }()
-            private var cancellable: Set<AnyCancellable> = []
+    }
+}
 
-            private func style() {
-                layer.cornerRadius = 10
-                layer.masksToBounds = true
-                backgroundColor = Color.subNavy2.withAlphaComponent(0.8)
+extension TopicContentInputTableViewCell {
+    
+    class CustomImageView: UIImageView {
+        
+        init(option: Choice.Option) {
+            self.option = option
+            super.init(frame: .zero)
+            style()
+            hierarchy()
+            layout()
+            initialize()
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        //MARK: Output
+        
+        let option: Choice.Option
+        let imageSubject: CurrentValueSubject<UIImage?, Never> = CurrentValueSubject(nil)
+        
+        private let uploadLabel: UILabel = {
+            let label = UILabel()
+            label.text = "업로드"
+            label.textColor = Color.subPurple
+            label.setTypo(Pretendard.semibold14)
+            return label
+        }()
+        private let optionLabel: UILabel = {
+            let label = UILabel()
+            label.setTypo(Pretendard.black100)
+            return label
+        }()
+        private let cancelButton: UIButton = {
+            let button = UIButton()
+            button.setImage(Image.topicGenerateImageCancel, for: .normal)
+            button.snp.makeConstraints{
+                $0.width.height.equalTo(18)
             }
-            
-            private func hierarchy() {
-                addSubviews([uploadLabel, optionLabel, cancelButton])
+            return button
+        }()
+        private var cancellable: Set<AnyCancellable> = []
+        
+        private func style() {
+            layer.cornerRadius = 10
+            layer.masksToBounds = true
+            backgroundColor = Color.subNavy2.withAlphaComponent(0.8)
+        }
+        
+        private func hierarchy() {
+            addSubviews([uploadLabel, optionLabel, cancelButton])
+        }
+        
+        private func layout() {
+            self.snp.makeConstraints{
+                $0.width.height.equalTo(90)
             }
-            
-            private func layout() {
-                self.snp.makeConstraints{
-                    $0.width.height.equalTo(90)
-                }
-                uploadLabel.snp.makeConstraints{
-                    $0.top.equalToSuperview().offset(8)
-                    $0.centerX.equalToSuperview()
-                }
-                optionLabel.snp.makeConstraints{
-                    $0.top.equalToSuperview().offset(12)
-                    $0.centerX.equalToSuperview()
-                }
-                cancelButton.snp.makeConstraints{
-                    $0.top.equalToSuperview().offset(8)
-                    $0.centerX.equalToSuperview()
-                }
+            uploadLabel.snp.makeConstraints{
+                $0.top.equalToSuperview().offset(8)
+                $0.centerX.equalToSuperview()
             }
-            
-            private func initialize() {
-                
-                optionLabel.text = option.content.title
-                optionLabel.textColor = option.content.color.withAlphaComponent(0.4)
-                addTarget()
-                addSubscriber()
-                
-                func addTarget() {
-                    cancelButton.tapPublisher
-                        .sink{ [weak self] _ in
-                            self?.imageSubject.send(nil)
-                        }
-                        .store(in: &cancellable)
-                }
-                
-                func addSubscriber() {
-                    imageSubject
-                        .receive(on: DispatchQueue.main)
-                        .sink{ [weak self] image in
-                            self?.image = image
-                            self?.setComponentVisibility()
-                        }
-                        .store(in: &cancellable)
-                }
+            optionLabel.snp.makeConstraints{
+                $0.top.equalToSuperview().offset(12)
+                $0.centerX.equalToSuperview()
             }
-            
-            func setComponentVisibility() {
-                if image == nil {
-                    uploadLabel.isHidden = false
-                    cancelButton.isHidden = true
-                }
-                else {
-                    uploadLabel.isHidden = true
-                    cancelButton.isHidden = false
-                }
+            cancelButton.snp.makeConstraints{
+                $0.top.equalToSuperview().offset(8)
+                $0.centerX.equalToSuperview()
             }
         }
         
-        class ImageContentTextField: CustomTextFieldView{
+        private func initialize() {
             
-            init(option: Choice.Option) {
-                super.init(placeholder: "이미지를 간단히 설명해주세요", insets: UIEdgeInsets(top: 16, left: 35, bottom: 16, right: 40), isErrorNeed: false)
-                optionLabel.text = option.rawValue
-                optionLabel.textColor = option.content.color
+            setOptionLabelStyle()
+            addTarget()
+            addSubscriber()
+            
+            func setOptionLabelStyle() {
+                optionLabel.text = option.content.title
+                optionLabel.textColor = option.content.color.withAlphaComponent(0.4)
             }
             
-            required init(coder: NSCoder) {
-                fatalError("init(coder:) has not been implemented")
+            func addTarget() {
+                cancelButton.tapPublisher
+                    .sink{ [weak self] _ in
+                        self?.imageSubject.send(nil)
+                    }
+                    .store(in: &cancellable)
             }
             
-            private lazy var optionLabel: UILabel = {
-               let label = UILabel()
-                label.setTypo(Pretendard.semibold14)
-                addSubview(label)
-                label.snp.makeConstraints{
-                    $0.centerY.equalToSuperview()
-                    $0.leading.equalToSuperview().offset(16)
-                }
-                return label
-            }()
-            
-            override func style() {
-                super.style()
-                textField.backgroundColor = Color.subNavy2.withAlphaComponent(0.4)
-                textField.textColor = Color.white
-                customPlaceholder(color: Color.subPurple.withAlphaComponent(0.6), font: Pretendard.medium16.font)
-                countLabel.textColor = Color.subPurple.withAlphaComponent(0.6)
+            func addSubscriber() {
+                imageSubject
+                    .receive(on: DispatchQueue.main)
+                    .sink{ [weak self] image in
+                        self?.image = image
+                        self?.setComponentVisibility()
+                    }
+                    .store(in: &cancellable)
             }
+        }
+        
+        //MARK: Input
+        
+        func setComponentVisibility() {
+            if image == nil {
+                uploadLabel.isHidden = false
+                cancelButton.isHidden = true
+            }
+            else {
+                uploadLabel.isHidden = true
+                cancelButton.isHidden = false
+            }
+        }
+    }
+}
+extension TopicContentInputTableViewCell {
+    
+    class ImageContentTextField: CustomTextFieldView{
+        
+        init(option: Choice.Option) {
+            super.init(placeholder: "이미지를 간단히 설명해주세요", insets: UIEdgeInsets(top: 16, left: 35, bottom: 16, right: 40), isErrorNeed: false)
+            optionLabel.text = option.rawValue
+            optionLabel.textColor = option.content.color
+        }
+        
+        required init(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        private lazy var optionLabel: UILabel = {
+            let label = UILabel()
+            label.setTypo(Pretendard.semibold14)
+            addSubview(label)
+            label.snp.makeConstraints{
+                $0.centerY.equalToSuperview()
+                $0.leading.equalToSuperview().offset(16)
+            }
+            return label
+        }()
+        
+        override func style() {
+            super.style()
+            textField.backgroundColor = Color.subNavy2.withAlphaComponent(0.4)
+            textField.textColor = Color.white
+            customPlaceholder(color: Color.subPurple.withAlphaComponent(0.6), font: Pretendard.medium16.font)
+            countLabel.textColor = Color.subPurple.withAlphaComponent(0.6)
         }
     }
 }
