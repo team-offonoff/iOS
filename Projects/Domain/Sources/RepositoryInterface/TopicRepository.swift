98143e12444560ffc4495e2c861ab31c05c91f4c
SSOOYA <bsy24660@naver.com>
2023-12-31 15:50:52 +0900

    feat: 토픽 생성 유스케이스 모델 생성 및 적용 (#105)

--- Projects/Domain/Sources/RepositoryInterface/TopicRepository.swift
+++ Projects/Domain/Sources/RepositoryInterface/TopicRepository.swift
@@ -10,7 +10,7 @@ import Foundation
 import Core
 
 public protocol TopicRepository: Repository{
-    func generateTopic(request: Topic) -> NetworkResultPublisher<Topic?>
+    func generateTopic(request: GenerateTopicUseCaseRequestValue) -> NetworkResultPublisher<Topic?>
     func fetchTopic(keywordId: Int?, paging: Paging?, sort: String?) -> NetworkResultPublisher<(Paging, [Topic])?>
     func report(topicId: Int) -> NetworkResultPublisher<Any?>
     func vote(topicId: Int, request: GenerateVoteUseCaseRequestValue) -> NetworkResultPublisher<Any?>
