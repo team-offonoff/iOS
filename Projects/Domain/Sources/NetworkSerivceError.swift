7c1559d25e734b2a41a5a34eb026fb08adea21b7
SSOOYA <bsy24660@naver.com>
2024-01-05 15:56:30 +0900

    feat(Domain): NetworkSerivceError 케이스 추가

--- Projects/Domain/Sources/NetworkSerivceError.swift
+++ Projects/Domain/Sources/NetworkSerivceError.swift
@@ -9,11 +9,27 @@
 import Foundation
 
 public enum NetworkSerivceError: String {
+    
     case emptyAuthorization = "EMPTY_AUTHORIZATION"
-    case votedByAuthor = "VOTED_BY_AUTHOR"
-    case futureTimeRequest = "FUTURE_TIME_REQUEST"
-    case memberNotVote = "MEMBER_NOT_VOTE"
     case invalidField = "INVALID_FIELD"
-    case topicNotFound = "TOPIC_NOT_FOUND"
-    case duplicateTopicReport = "DUPLICATE_TOPIC_REPORT"
+    
+    //MARK: Topic
+    case TOPIC_NOT_FOUND
+    case DUPLICATE_TOPIC_REPORT
+    case ILLEGAL_TOPIC_STATUS_CHANGE
+    case VOTED_BY_AUTHOR
+    case FUTURE_TIME_REQUEST
+    case MEMBER_NOT_VOTE
+    case DUPLICATE_VOTE
+    
+    //MARK: Auth
+    case ILLEGAL_JOIN_STATUS
+    
+    //MARK: Comment
+    case INVALID_LENGTH_OF_FIELD
+    case UNABLE_TO_VIEW_COMMENTS
+    case ILLEGAL_COMMENT_STATUS_CHANGE
+    
+    //MARK: Image Presigned
+    case ILLEGAL_FILE_EXTENSION
 }
