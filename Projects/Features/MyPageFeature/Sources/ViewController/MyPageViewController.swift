361b7482bc9b1ca63fb5731711e77c04f75ccff9
SSOOYA <bsy24660@naver.com>
2024-01-16 17:55:18 +0900

    feat(ViewController): 마이페이지 프로필 변경/삭제 기능 구현 (#134)

--- Projects/Features/MyPageFeature/Sources/ViewController/MyPageViewController.swift
+++ Projects/Features/MyPageFeature/Sources/ViewController/MyPageViewController.swift
@@ -9,7 +9,10 @@
 import Foundation
 import UIKit
 import ABKit
+import MyPageFeatureInterface
 import FeatureDependency
+import Domain
+import PhotosUI
 
 final class MyPageViewController: BaseViewController<HeaderView, MyPageView, DefaultMyPageCoordinator> {
     
@@ -19,5 +22,63 @@ final class MyPageViewController: BaseViewController<HeaderView, MyPageView, Def
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private let viewModel: any MyPageViewModel
+    
+    override func initialize() {
+        
+        addGalleryNotificationObserver()
+        
+        mainView.imageModifyButton.tapPublisher
+            .sink{ [weak self] _ in
+                self?.coordinator?.startProfileImageActionBottomSheet()
+            }
+            .store(in: &cancellables)
+        
+        func addGalleryNotificationObserver() {
+            NotificationCenter.default.publisher(for: Notification.Name(Profile.Image.Action.gallery.identifier))
+                .sink{ [weak self] _ in
+                    //갤러리 보여주기
+                    self?.present(imagePicker(), animated: true)
+                    
+                    func imagePicker() -> PHPickerViewController {
+                        var configuration = PHPickerConfiguration()
+                        configuration.selectionLimit = 1
+                        configuration.filter = .images
+                        let picker = PHPickerViewController(configuration: configuration)
+                        picker.delegate = self
+                        return picker
+                    }
+                }
+                .store(in: &cancellables)
+        }
+    }
+    
+    override func bind() {
+        viewModel.profileImage
+            .receive(on: DispatchQueue.main)
+            .sink{ [weak self] url in
+                self?.mainView.profileImageView.image = nil
+            }
+            .store(in: &cancellables)
+    }
+}
+
+extension MyPageViewController: PHPickerViewControllerDelegate {
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+
+        picker.dismiss(animated: true) {
+            
+            guard let itemProvider = results.first?.itemProvider else { return }
+            
+            if itemProvider.canLoadObject(ofClass: UIImage.self) {
+                itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+                    guard let self = self, let image = image as? UIImage else { return }
+                    self.viewModel.modifyImage(image)
+                }
+            }
+        }
     }
 }
