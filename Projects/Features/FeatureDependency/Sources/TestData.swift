bc64831073ab7ed6016addb1feacbf8cb7eaf0f9
SSOOYA <bsy24660@naver.com>
2024-02-03 20:27:31 +0900

    feat: 토픽 테스트 데이터 주석 처리 (#139)

--- Projects/Features/FeatureDependency/Sources/TestData.swift
+++ Projects/Features/FeatureDependency/Sources/TestData.swift
@@ -12,63 +12,63 @@ import Core
 
 public struct TestData {
     
-    public static let topicData1 = Topic(
-        id: 0,
-        side: .A,
-        title: "10년 전 또는 후로 갈 수 있다면?",
-        deadline: UTCTime.current + 10,
-        voteCount: 100,
-        keyword: .init(id: 0, name: "키워드", topicSide: .A),
-        choices: [
-            Choice(id: 0, content: Choice.Content(text: "10년 전 과거로 가기", imageURL: nil), option: .A),
-            Choice(id: 1, content: Choice.Content(text: "10년 후 미래로 가기", imageURL: nil), option: .B)
-        ],
-        author: .init(id: 0, nickname: "닉네임", profileImageUrl: nil),
-        selectedOption: nil
-    )
-    
-    public static let topicData2 = Topic(
-        id: 0,
-        side: .A,
-        title: "10년 전 또는 후로 갈 수 있다면?",
-        deadline: UTCTime.current + 24*60,
-        voteCount: 100,
-        keyword: .init(id: 0, name: "키워드", topicSide: .A),
-        choices: [
-            Choice(id: 0, content: Choice.Content(text: "10년 전 과거로 가기", imageURL: nil), option: .A),
-            Choice(id: 1, content: Choice.Content(text: "10년 후 미래로 가기", imageURL: nil), option: .B)
-        ],
-        author: .init(id: 0, nickname: "닉네임", profileImageUrl: nil),
-        selectedOption: .A
-    )
-    
-    public static let topicData3 = Topic(
-        id: 0,
-        side: .A,
-        title: "10년 전 또는 후로 갈 수 있다면?",
-        deadline: UTCTime.current + 24*60,
-        voteCount: 100,
-        keyword: .init(id: 0, name: "키워드", topicSide: .A),
-        choices: [
-            Choice(id: 0, content: Choice.Content(text: "10년 전 과거로 가기", imageURL: nil), option: .A),
-            Choice(id: 1, content: Choice.Content(text: "10년 후 미래로 가기", imageURL: nil), option: .B)
-        ],
-        author: .init(id: 0, nickname: "닉네임", profileImageUrl: nil),
-        selectedOption: .B
-    )
-    
-    public static let topicData4 = Topic(
-        id: 0,
-        side: .A,
-        title: "10년 전 또는 후로 갈 수 있다면?",
-        deadline: UTCTime.current + 24*60,
-        voteCount: 100,
-        keyword: .init(id: 0, name: "키워드", topicSide: .A),
-        choices: [
-            Choice(id: 0, content: Choice.Content(text: "10년 전 과거로 가기", imageURL: URL(string: "http://ab")), option: .A),
-            Choice(id: 1, content: Choice.Content(text: "10년 후 미래로 가기", imageURL: URL(string: "http://ab")), option: .B)
-        ],
-        author: .init(id: 0, nickname: "닉네임", profileImageUrl: nil),
-        selectedOption: .B
-    )
+//    public static let topicData1 = Topic(
+//        id: 0,
+//        side: .A,
+//        title: "10년 전 또는 후로 갈 수 있다면?",
+//        deadline: UTCTime.current + 10,
+//        voteCount: 100,
+//        keyword: .init(id: 0, name: "키워드", topicSide: .A),
+//        choices: [
+//            Choice(id: 0, content: Choice.Content(text: "10년 전 과거로 가기", imageURL: nil), option: .A),
+//            Choice(id: 1, content: Choice.Content(text: "10년 후 미래로 가기", imageURL: nil), option: .B)
+//        ],
+//        author: .init(id: 0, nickname: "닉네임", profileImageUrl: nil),
+//        selectedOption: nil
+//    )
+//
+//    public static let topicData2 = Topic(
+//        id: 0,
+//        side: .A,
+//        title: "10년 전 또는 후로 갈 수 있다면?",
+//        deadline: UTCTime.current + 24*60,
+//        voteCount: 100,
+//        keyword: .init(id: 0, name: "키워드", topicSide: .A),
+//        choices: [
+//            Choice(id: 0, content: Choice.Content(text: "10년 전 과거로 가기", imageURL: nil), option: .A),
+//            Choice(id: 1, content: Choice.Content(text: "10년 후 미래로 가기", imageURL: nil), option: .B)
+//        ],
+//        author: .init(id: 0, nickname: "닉네임", profileImageUrl: nil),
+//        selectedOption: .A
+//    )
+//
+//    public static let topicData3 = Topic(
+//        id: 0,
+//        side: .A,
+//        title: "10년 전 또는 후로 갈 수 있다면?",
+//        deadline: UTCTime.current + 24*60,
+//        voteCount: 100,
+//        keyword: .init(id: 0, name: "키워드", topicSide: .A),
+//        choices: [
+//            Choice(id: 0, content: Choice.Content(text: "10년 전 과거로 가기", imageURL: nil), option: .A),
+//            Choice(id: 1, content: Choice.Content(text: "10년 후 미래로 가기", imageURL: nil), option: .B)
+//        ],
+//        author: .init(id: 0, nickname: "닉네임", profileImageUrl: nil),
+//        selectedOption: .B
+//    )
+//
+//    public static let topicData4 = Topic(
+//        id: 0,
+//        side: .A,
+//        title: "10년 전 또는 후로 갈 수 있다면?",
+//        deadline: UTCTime.current + 24*60,
+//        voteCount: 100,
+//        keyword: .init(id: 0, name: "키워드", topicSide: .A),
+//        choices: [
+//            Choice(id: 0, content: Choice.Content(text: "10년 전 과거로 가기", imageURL: URL(string: "http://ab")), option: .A),
+//            Choice(id: 1, content: Choice.Content(text: "10년 후 미래로 가기", imageURL: URL(string: "http://ab")), option: .B)
+//        ],
+//        author: .init(id: 0, nickname: "닉네임", profileImageUrl: nil),
+//        selectedOption: .B
+//    )
 }
